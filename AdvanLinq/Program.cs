953568d5af3e5f53aed8dd7d7cc3a27e1d3a6b42
Gau2209 <anhtuanvo2209@gmail.com>
2025-09-28 13:47:45 +0700

    Take và Skip

--- AdvanLinq/Program.cs
+++ AdvanLinq/Program.cs
@@ -26,7 +26,7 @@
 
             #region Join
             //// Join: Kết hợp hai tập hợp dựa trên một khóa chung.
-            
+
 
 
             //var query = from student in Students
@@ -41,19 +41,43 @@
             #endregion
 
             #region Left Join
-            // Left Join: Giống như Join, nhưng bao gồm tất cả các phần tử từ tập hợp bên trái, ngay cả khi không có phần tử tương ứng trong tập hợp bên phải.
+            //// Left Join: Giống như Join, nhưng bao gồm tất cả các phần tử từ tập hợp bên trái, ngay cả khi không có phần tử tương ứng trong tập hợp bên phải.
 
-            var leftJoinQuery = from student in Students
-                                join score in Scores 
-                                on student.ID equals score.StudentID into studentScores
-                                from score in studentScores.DefaultIfEmpty()
-                                select new { StudentID = student.ID, Name = student.Name, Score = score?.Score ?? 0 };
+            //var leftJoinQuery = from student in Students
+            //                    join score in Scores 
+            //                    on student.ID equals score.StudentID into studentScores
+            //                    from score in studentScores.DefaultIfEmpty()
+            //                    select new { StudentID = student.ID, Name = student.Name, Score = score?.Score ?? 0 };
 
-            foreach (var s in leftJoinQuery)
-            {
-                Console.WriteLine($"{s.StudentID} - {s.Name} - {s.Score}");
-            }
+            //foreach (var s in leftJoinQuery)
+            //{
+            //    Console.WriteLine($"{s.StudentID} - {s.Name} - {s.Score}");
+            //}
 
+            #endregion
+
+            #region Take và Skip
+            // Take và Skip: Lấy hoặc bỏ qua một số phần tử từ tập hợp.
+            var ListNumber = Enumerable.Range(1, 20);
+            var takeQuery = ListNumber.Take(5); // Lấy 5 phần tử đầu tiên
+            Console.WriteLine("Take:" + string.Join(", ",takeQuery));
+
+            var skipQuery = ListNumber.Skip(10); // Bỏ qua 10 phần tử đầu tiên
+            Console.WriteLine("Skip: " + string.Join(", ", skipQuery));
+
+            var takeLastQuery = ListNumber.TakeLast(5); // Lấy 5 phần tử cuối cùng
+            Console.WriteLine("Take last: " +string.Join(", ", takeLastQuery));
+
+            var skipLastQuery = ListNumber.SkipLast(10); // Bỏ qua 10 phần tử cuối cùng
+
+            Console.WriteLine("Skip last: " +string.Join(", ", skipLastQuery));
+
+            var takeWhileQuery = ListNumber.TakeWhile(n => n < 10); // Lấy phần từ nhỏ hơn 10
+            Console.WriteLine("Take while: " +string.Join(", ", takeWhileQuery));
+
+            var skipWhileQuery = ListNumber.SkipWhile(n => n < 10); // Bỏ qua phần tử nhỏ hơn 10
+
+            Console.WriteLine("Skip while: " +string.Join(", ", skipWhileQuery));
             #endregion
         }
     }
