8e56914f9de3653045326b9ea6fc96c132d3a88a
Gau2209 <anhtuanvo2209@gmail.com>
2025-09-28 13:38:04 +0700

    Left Join

--- AdvanLinq/Program.cs
+++ AdvanLinq/Program.cs
@@ -6,8 +6,9 @@
         {
             Console.InputEncoding = System.Text.Encoding.UTF8;
             Console.OutputEncoding = System.Text.Encoding.UTF8;
+
             var Students = new[]
-                        {
+            {
                 new {ID = 1, Name = "Anh Tuấn" },
                 new {ID = 2, Name = "Hoàng" },
                 new {ID = 3, Name = "Hẹ hẹ" },
@@ -21,19 +22,39 @@
                 new {StudentID = 2, Score = 5.7f },
                 new {StudentID = 3, Score = 5.8f },
                 new {StudentID = 4, Score = 5.9f },
-                new {StudentID = 5, Score = 6f },
             };
 
-            var query = from student in Students
-                        join score in Scores
-                        on student.ID equals score.StudentID
-                        select new { StudentID = student.ID, Name = student.Name, Score = score.Score };
+            #region Join
+            //// Join: Kết hợp hai tập hợp dựa trên một khóa chung.
+            
 
-            foreach (var s in query)
+
+            //var query = from student in Students
+            //            join score in Scores
+            //            on student.ID equals score.StudentID
+            //            select new { StudentID = student.ID, Name = student.Name, Score = score.Score };
+
+            //foreach (var s in query)
+            //{
+            //    Console.WriteLine($"{s.StudentID} - {s.Name} - {s.Score}");
+            //}
+            #endregion
+
+            #region Left Join
+            // Left Join: Giống như Join, nhưng bao gồm tất cả các phần tử từ tập hợp bên trái, ngay cả khi không có phần tử tương ứng trong tập hợp bên phải.
+
+            var leftJoinQuery = from student in Students
+                                join score in Scores 
+                                on student.ID equals score.StudentID into studentScores
+                                from score in studentScores.DefaultIfEmpty()
+                                select new { StudentID = student.ID, Name = student.Name, Score = score?.Score ?? 0 };
+
+            foreach (var s in leftJoinQuery)
             {
                 Console.WriteLine($"{s.StudentID} - {s.Name} - {s.Score}");
             }
 
+            #endregion
         }
     }
 }
